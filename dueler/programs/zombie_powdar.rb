d80b75c68d0145abf6626c6ce7afc93f9191cd28
Narihiro Nakamura <authornari@gmail.com>
2011-06-19 14:42:04 +0900

    helper追加

--- dueler/programs/zombie_powdar.rb
+++ dueler/programs/zombie_powdar.rb
@@ -1,18 +1,9 @@
 #!/usr/bin/env ruby
 # -*- coding: utf-8 -*-
-require_relative "num_maker"
+require_relative "helper"
 
 # 指定するスロットには対象のcardを入れておかなければならない。
 # 引数に数値をbindする
-def bind(slot, num)
-  o "put", 0
-  make_about_num(0, num)
-  o "K", slot      # s: K(card)
-  o "S", slot      # s: S(K(card))
-  o slot, "get"    # s: S(K(card))(get)
-  o slot, "zero"   # s: S(K(card))(get)(zero) => card(i)
-end
-
 def send_zomie_help(i, j, target_slot)
   o 1, "help"   # s: help
   bind(1, i)
@@ -29,7 +20,6 @@ def send_zomie_help(i, j, target_slot)
   # o 2, "get"    # s: S(K(help))(get)
   # o 2, "zero"   # s: S(K(help))(get)(zero) => help(i)
 end
-
 
 # [:I]を受け取ったときに強力なhelp or attackを呼び出したい
 # help(i)(j)(n)

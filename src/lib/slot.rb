10fc104cc90beb47aca148fdbe5ae085bb33ac57
Kouji Takao <kouji@takao7.net>
2011-06-20 00:44:06 +0900

    デバッグ出力の修正をしたかったがまにあわなかった。コメントを削除した。

--- src/lib/slot.rb
+++ src/lib/slot.rb
@@ -34,8 +34,6 @@ class Slot
   end
 
   def to_s
-    s = field.to_s
-    #s = field.to_s.gsub(/\[:/, "(").gsub(/\]/, ")").gsub(/, /, "").sub(/^\(/, "").sub(/\)$/, "")
-    return "#{slot_no}={#{vitality},#{s}}"
+    return "#{slot_no}={#{vitality},#{field}}"
   end
 end

0320e9eebb0a3a86c2266b861ccf0dee2391226c
Kouji Takao <kouji@takao7.net>
2011-06-19 23:04:38 +0900

    halperを使うように修正。

--- dueler/programs/do_nothing.rb
+++ dueler/programs/do_nothing.rb
@@ -1,25 +1,6 @@
 #!/usr/bin/env ruby
-$stdout.sync = true
+require_relative "helper"
 
-def opp
-  case $stdin.gets
-  when "1\n"
-    card = $stdin.gets
-    slot = $stdin.gets
-  when "2\n"
-    slot = $stdin.gets
-    card = $stdin.gets
-  else
-    exit
-  end
-end
-
-#$stderr.puts "****#{ARGV.inspect}****"
-
-opp if ARGV[0] == "1"
 loop do
-  puts "1"
-  puts "I"
-  puts "0"
-  opp
+  o "I", 0
 end

0e047bb180fab7ab0aac29d96ab1f6a96b00ebd6
Yutaka HARA <yutaka.hara.gmail.com>
2011-06-19 23:30:46 +0900

    ゾンビにhelp(copy(a))(copy(b))(copy(c))を実行させる例

--- dueler/programs/copy_zombie.rb
+++ dueler/programs/copy_zombie.rb
@@ -12,23 +12,61 @@ help_10 みたいな重たい関数があるとして、
 
 dec(copy(3)) みたいな
 
-S[K(dec)][copy_3][I]
-= dec( 11 )
+  S(K(dec))(S(K(copy))(K(3)))(I)
+  = K(dec)(I) ( S(K(copy))(K(3))(I) )
+  = dec ( K(copy)(I)( K(3)(I) ))
+  = dec ( copy(3) )
 
-copy_3 = S[K(copy)][K(3)] (引数を渡すとcopy(3)を実行する)
+ではhelpでは？
 
-help(i)(j)(n)
+help(copy_3)(copy_4)(copy_5) を実行させたい。
 
-help(copy_3)(copy_4)(copy_5)
 
+  S(K(_))(S(K(copy))(K(5)))(I)
+  = _(copy(5))
+
+  S(K<help(copy(3))(copy(4))>)(S(K(copy))(K(5)))(I)
+  = help(copy(3))(copy(4))>)(S(K(copy))(K(5)))(I)
+
+S[
+  S{ S[K(help)][ S(K(copy))(K(3)) ] }{ S(K(copy))(K(4)) }
+ ]
+ [ 
+  S(K(copy))(K(5)) 
+ ]
 
 --
-S(K(dec))(S(K(copy))(K(3)))(I)
-= K(dec)(I) ( S(K(copy))(K(3))(I) )
-= dec ( K(copy)(I)( K(3)(I) ))
-= dec ( copy(3) )
 
 =end
+
+def set_lazy_copy(slot, tmp, param)
+  make_num tmp, param     # tmp: 5
+  o "K", tmp              # tmp: K(5)
+
+  set slot, S[K[copy]]    # slot: S[K[copy]]
+  bind_func slot, tmp     # slot: S[K[copy][K(5)]]
+end
+
+def help_for_zombie(slot, tmp, tmp2, param1, param2, param3)
+#  S[
+#    S{ S[K(help)][ S(K(copy))(K(3)) ] }{ S(K(copy))(K(4)) }
+#   ]
+#   [ 
+#    S(K(copy))(K(5)) 
+#   ]
+
+  set_lazy_copy tmp, tmp2, param1   # tmp: S(K(copy))(K(3))
+  set slot, S[K[help]]              # slot:   S[K[help]]
+  bind_func slot, tmp               # slot:   S[K[help]][ S(K(copy))(K(3)) ]
+  o "S", slot                       # slot: S{S[K[help]][ S(K(copy))(K(3)) ]}
+
+  set_lazy_copy tmp, tmp2, param2   # tmp: S(K(copy))(K(4))
+  bind_func slot, tmp               # slot:    S{S[K[help]][ S(K(copy))(K(3)) ]}{ S(K(copy))(K(4)) }
+  o "S", slot                       # slot: S[ S{S[K[help]][ S(K(copy))(K(3)) ]}{ S(K(copy))(K(4)) } ]
+
+  set_lazy_copy tmp, tmp2, param3   # tmp: S(K(copy))(K(5))
+  bind_func slot, tmp               # slot: S[ ... ][ S(K(copy))(K(5)) ]
+end
 
 def inc_for_zombie(slot, tmp, param1=3)
   make_num param1, 77
@@ -53,14 +91,12 @@ end
 #  bind(1, 10000, apply_to_zero: ["K"]) # 1: S(K(help(1)(2)))(K(16384))
 #end
 
-def zomie_powder(target_slot, opts)
-  o "put", 2
-  o 2, "zombie"        # 2: zombie
-  bind 2, target_slot  # 2: zombie(target_slot)
-  bind_func 2, opts[:func]
-
-  #set 2, S[K[S[K[_now_]][get]]][succ][zero][zero]
-
+def zomie_powder(opts) # opts = :func, :target, :tmp
+  tmp = opts[:tmp]
+  o "put", tmp
+  o tmp, "zombie"        # 2: zombie
+  bind tmp, (255 - opts[:target])  # 2: zombie(target_slot)
+  bind_func tmp, opts[:func]
 end
 
 def attack(i, j, n)
@@ -79,14 +115,17 @@ $stderr.puts "@"*128
 target = 0
 #target.step(2550, 2) do |i|
 1.times{
-  inc_for_zombie(1, 5)
+  help_for_zombie(1, 2, 3, 4, 5, 6)
 
-  make_num 3, 71
-  zomie_powder(0, func: 1)
-  make_num 3, 81
-  zomie_powder(0, func: 1)
-  make_num 3, 91
-  zomie_powder(0, func: 1)
+  make_num 4, 11
+  make_num 5, 12
+  make_num 6, 13
+  zomie_powder(target: 255, func: 1, tmp: 2)
+
+  make_num 4, 21
+  make_num 5, 22
+  make_num 6, 33
+  zomie_powder(target: 255, func: 1, tmp: 2)
 }
 o "I", 255
 o "I", 255

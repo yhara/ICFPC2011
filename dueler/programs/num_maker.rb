36de45e0b884047a43f24c72b4b9bd08f6eab06d
Narihiro Nakamura <authornari@gmail.com>
2011-06-19 12:48:32 +0900

    数値を作るヤツを少し修正＆コメント

--- dueler/programs/num_maker.rb
+++ dueler/programs/num_maker.rb
@@ -13,17 +13,28 @@ def o(arg1, arg2)
   command apply, arg1, arg2
 end
 
-def self.make_num(slot, num)
+# スロット番号を生成する場合、2**nの位置を指定するとターン数を減らせる
+def make_about_num(slot, num)
   o slot, "zero" # => 0
   o "succ", slot # => 1
   dbl_cnt = Math::log2(num).to_i
   dbl_cnt.times do
     o "dbl", slot
   end
-  succ_cnt = num - (1 << dbl_cnt)
-  succ_cnt.times do
-    o "succ", slot
+  # スロット番号を生成する場合は精度を高める。
+  if num < 255
+    succ_cnt = num - (1 << dbl_cnt)
+    succ_cnt.times do
+      o "succ", slot
+    end
+  else
+    # それ以外は指定の数に近いn**2を生成
+    now_num = (num - (1 << dbl_cnt)).abs
+    onemore_dbl_num = (num - (1 << (dbl_cnt+1))).abs
+    if now_num > onemore_dbl_num
+      o "dbl", slot
+    end
   end
 end
 
-make_num(100, 100)
+make_about_num(100, 10000) # => 17ターンで生成することが可能

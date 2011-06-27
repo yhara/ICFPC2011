fd35e98f0545132f65f8c48404ba1ccb7a3be730
Yutaka HARA <yutaka.hara.gmail.com>
2011-06-27 16:20:15 +0900

    iに数値以外が渡されたときに予期せぬ例外になるのを修正

--- src/lib/slots.rb
+++ src/lib/slots.rb
@@ -5,6 +5,7 @@ class Slots < Array
   # i番目のスロットを取得する。iは0から255。
   # 範囲外のスロットへのアクセス時にルールとしてエラーを発生させる。
   def [](i)
+    raise IndexNativeError, "#{i.class} #{i.inspect} is not a Fixnum" if not i.is_a?(Fixnum)
     raise IndexNativeError, "index #{i} outside of slots" if i < 0 || i >= length
     return fetch(i)
   end
